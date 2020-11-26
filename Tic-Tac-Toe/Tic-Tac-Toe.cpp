3bee60f45e0e35d73e0af01aca35f3ca2c00ee83
Egor Semenov <EDSemenov19@codingburgas.bg>
2020-11-26 17:22:41 +0200

    Create mainMenu function

--- Tic-Tac-Toe/Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe/Tic-Tac-Toe.cpp
@@ -1,20 +1,41 @@
-// Tic-Tac-Toe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+bool mainMenu()
+{
+	char menuOption;
+	cout << "Main Menu" << endl;
+	cout << "Type 1 to Play the Game" << endl;
+	cout << "Type 0 to Exit the Game" << endl;
+
+	cin >> menuOption;
+
+	switch (menuOption)
+
+	{
+	case '1': 
+		return true;
+		break;
+
+	case '0':
+		return false;
+		break;
+
+	default: 
+		mainMenu();
+		break;
+	}
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int ticTacToeSquares[9] = { 0,0,0,0,0,0,0,0,0 };
+	bool exitProgram;
+	do {
+		exitProgram=mainMenu();
+	} while (exitProgram);
+	
+
+	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
